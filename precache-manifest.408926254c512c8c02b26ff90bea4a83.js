2f9dfe0357ed650cae1cb9ea06fcce81348ae16b
hosam8081 <hosamsalah8081@gmail.com>
2022-08-21 00:55:44 +0200

    Updates

---
+++ precache-manifest.408926254c512c8c02b26ff90bea4a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e8a746c4f8e87f00020bf4e755dff3",
+    "url": "/quizz/index.html"
+  },
+  {
+    "revision": "08b1edbc1b6f88bc58fe",
+    "url": "/quizz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "11104653227ded0d437f",
+    "url": "/quizz/static/js/2.a09da955.chunk.js"
+  },
+  {
+    "revision": "ab8f792973d32f9b93c607ee81282492",
+    "url": "/quizz/static/js/2.a09da955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08b1edbc1b6f88bc58fe",
+    "url": "/quizz/static/js/main.9f4a3de9.chunk.js"
+  },
+  {
+    "revision": "5aae57ea387f66d21a4d",
+    "url": "/quizz/static/js/runtime-main.deb5236c.js"
+  }
+]);
